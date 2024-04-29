cff34bb713aad9c14a2302411eb86231a6e09b6f
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2024-04-29 13:14:54 -0400

    added more images and styles, added flexbox

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import './styles/style.css';
 import Icon from './images/maltese_cross.png'
 import { home } from "./home";
 
-console.log("Hello there")
-
-
 const header = document.querySelector('header')
 const myLogo = new Image();
+const myLogo2 = new Image();
 myLogo.src = Icon;
+myLogo2.src = Icon;
+myLogo.classList.add("logo");
+myLogo2.classList.add("logo");
 header.prepend(myLogo);
+header.append(myLogo2);
